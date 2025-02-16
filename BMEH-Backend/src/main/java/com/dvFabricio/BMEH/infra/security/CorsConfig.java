92df1f151c033610a2b49800b3288bdc9857faa2
Fabricio <fa.engeroff1996@gmail.com>
2025-02-16 10:22:46 -0300

    Adicionado os campos endereço, cpf e telefone nas migratinos.

--- BMEH-Backend/src/main/java/com/dvFabricio/BMEH/infra/security/CorsConfig.java
+++ BMEH-Backend/src/main/java/com/dvFabricio/BMEH/infra/security/CorsConfig.java
@@ -10,14 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "http://localhost:5176",
-                        "http://127.0.0.1:5176"
-                )
+                .allowedOrigins("http://localhost:5176")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+
     }
 }
 
--- BMEH-Backend/src/main/resources/db/migration/V2__create-table-users.sql
+++ BMEH-Backend/src/main/resources/db/migration/V2__create-table-users.sql
@@ -2,13 +2,29 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     login VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    cpf VARCHAR(14) UNIQUE NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    rua VARCHAR(255) NOT NULL,
+    numero VARCHAR(10) NOT NULL,
+    bairro VARCHAR(255) NOT NULL,
+    cidade VARCHAR(255) NOT NULL,
+    estado VARCHAR(2) NOT NULL,
+    cep VARCHAR(10) NOT NULL
 );
 
-INSERT INTO users (id, login, email, password)
+INSERT INTO users (id, login, email, password, cpf, telefone, rua, numero, bairro, cidade, estado, cep)
 VALUES (
     'd193afd4-9222-4150-aadb-5167405a771c',
     'newuser',
     'newuser@example.com',
-    '$2a$10$3yHNGw3SkUYZECFGm3N9tOmXWQiS.K5/VYj3wVlTZzDrMGo5q6fRu'
+    '$2a$10$3yHNGw3SkUYZECFGm3N9tOmXWQiS.K5/VYj3wVlTZzDrMGo5q6fRu',
+    '12345678901',
+    '11999999999',
+    'Rua A',
+    '123',
+    'Bairro B',
+    'Cidade C',
+    'SP',
+    '01001000'
 );
